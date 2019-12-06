d54296a529c4dbf879c98331c0d6c1a7b585f8ac
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-12-06 08:56:00 -0800

    Finishes ex2 from the hashtables!

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -27,6 +27,8 @@ def reconstruct_trip(tickets, length):
             route[current]=ticket.destination
             current+=1
 
-    
+    while current<length:
+        route[current]=hash_table_retrieve(hashtable,route[current-1])
+        current+=1
 
     return route
